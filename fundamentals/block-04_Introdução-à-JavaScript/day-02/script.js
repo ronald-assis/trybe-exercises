6053df38599b769b48c20a5b9d0063362bd151ed
Ronald Assis <ronaldassis71@gmail.com>
2021-08-26 16:28:40 -0300

    exercise 4.2

---
+++ fundamentals/block-04_Introdução-à-JavaScript/day-02/script.js
@@ -0,0 +1,48 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+let menuServices = menu[1]
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+menu.push('Contato')
+
+// console.log(menuServices)
+// console.log(indexOfPortfolio);
+
+for(let i = 0; i < menu.length; i+= 1){
+    // console.log(menu[i])
+}
+
+
+timesTables = 9
+
+for(let i = 0; i <= 9; i += 1){
+    // console.log(timesTables * i)
+}
+
+let nameList = ['Ronald', 'Ingrid', 'Vinicius']
+
+for(i = 0; i < nameList.length; i += 1){
+    let messagen = `Boas vindas ${nameList[i]} !`
+    // console.log(messagen)
+}
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let i = 0; i < groceryList.length; i+=1){
+    // console.log(groceryList[i])
+}
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let name of names){
+    // console.log(name)
+}
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 1
+
+for(let i = 0; i < numbers.length; i += 1 ){
+  sum = sum * numbers[i] 
+    console.log(sum)   
+}
