046d5fed3529eb85536626fb3ea21aa57912f1e7
Ronald Assis <ronaldassis71@gmail.com>
2021-08-27 22:05:57 -0300

    exercise 4.3

--- fundamentals/block-04_Introdução-à-JavaScript/day-03/script.js
+++ fundamentals/block-04_Introdução-à-JavaScript/day-03/script.js
@@ -1,14 +1,35 @@
-let fruits = [3, 4, 10, 1, 12]
+// let fruits = [3, 4, 10, 1, 12]
 
-let sum = 0
+// let sum = 0
 
-for(i = 0; i < fruits.length; i += 1){
-  sum += fruits[i]
-}
+// for(i = 0; i < fruits.length; i += 1){
+//   sum += fruits[i]
+// }
 
-if(sum > 15){
-  console.log(sum)
+// if(sum > 15){
+//   console.log(sum)
   
-}else if(sum <= 15){
-  console.log('Valor menor que 16')
+// }else if(sum <= 15){
+//   console.log('Valor menor que 16')
+// }
+
+
+
+// let asterisk = false
+
+let n = 3
+let asterisk = '*'
+let linha = ''
+// criar um laço de repetição para adicionar os '*' até 'n'
+
+// for (let l = 0; l < n; l+=1){  
+//   linha+= asterisk
+// }
+// for(let c = 0; c< n; c+=1){
+  // }
+  
+  
+  for (let l = 0; l <= n; l+=1){  
+    linha+= asterisk
+    console.log(linha)
 }
