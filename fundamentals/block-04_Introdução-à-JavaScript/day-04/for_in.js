4d2f9e509a4136bf9f3c2462c697cf0f56841f36
Ronald Assis <ronaldassis71@gmail.com>
2021-08-30 15:20:47 -0300

    ojects and for/in

---
+++ fundamentals/block-04_Introdução-à-JavaScript/day-04/for_in.js
@@ -0,0 +1,18 @@
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+};
+
+// for( name in names){
+//   console.log(`Olá ${names[name]}`)
+// }
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+for(key in car){
+  console.log(car)
+}
