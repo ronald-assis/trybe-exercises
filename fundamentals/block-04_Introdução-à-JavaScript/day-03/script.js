cc3aeaaf9a4848d5e2a036857a0339ae22b44747
Ronald Assis <ronaldassis71@gmail.com>
2021-08-30 14:30:14 -0300

    exersice 4.3

--- fundamentals/block-04_Introdução-à-JavaScript/day-03/script.js
+++ fundamentals/block-04_Introdução-à-JavaScript/day-03/script.js
@@ -17,19 +17,33 @@
 
 // let asterisk = false
 
-let n = 3
-let asterisk = '*'
-let linha = ''
 // criar um laço de repetição para adicionar os '*' até 'n'
 
 // for (let l = 0; l < n; l+=1){  
-//   linha+= asterisk
-// }
-// for(let c = 0; c< n; c+=1){
+  //   linha+= asterisk
   // }
-  
-  
-  for (let l = 0; l <= n; l+=1){  
-    linha+= asterisk
-    console.log(linha)
+  // for(let c = 0; c< n; c+=1){
+    // }
+    
+let n = 5
+let asterisk = '*'
+let linha = ''
+let midOfMatrix = (n + 1) / 2
+let controlLeft = midOfMatrix
+let controlRight = midOfMatrix
+
+for (let l = 0; l <= midOfMatrix; l+=1){  
+  for (let c = 0; c <=n; c+=1){
+     if(c > controlLeft && c < controlRight){
+       linha += asterisk
+     }else{
+      linha += ' '
+     }
+  }
+  console.log(linha)
+  linha+= '';
+ controlRight += 1
+ controlLeft -+ 1
 }
+
+
