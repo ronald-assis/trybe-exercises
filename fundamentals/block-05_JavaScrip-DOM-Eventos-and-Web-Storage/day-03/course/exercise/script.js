1b9225a443fda24d37d89580f41b18d4023a51d9
Ronald Assis <ronaldassis71@gmail.com>
2021-09-08 19:57:59 -0300

    applying days

--- fundamentals/block-05_JavaScrip-DOM-Eventos-and-Web-Storage/day-03/course/exercise/script.js
+++ fundamentals/block-05_JavaScrip-DOM-Eventos-and-Web-Storage/day-03/course/exercise/script.js
@@ -13,3 +13,19 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const classDays = document.querySelector('#days')
+
+
+function createEveryDay() {
+  for (let i = 0; i < dezDaysList.length; i += 1){
+    const numDay = dezDaysList[i]
+    const numDayList = document.createElement('li')
+    numDayList.classList.add('day')
+    numDayList.innerHTML = numDay
+
+    classDays.appendChild(numDayList)
+  }
+}
+
+createEveryDay()
