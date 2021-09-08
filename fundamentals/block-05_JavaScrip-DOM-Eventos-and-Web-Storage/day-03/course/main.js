d76113dfb831c99e9807334540c0d1d85e94bf35
Ronald Assis <ronaldassis71@gmail.com>
2021-09-08 16:20:38 -0300

    classTech

--- fundamentals/block-05_JavaScrip-DOM-Eventos-and-Web-Storage/day-03/course/main.js
+++ fundamentals/block-05_JavaScrip-DOM-Eventos-and-Web-Storage/day-03/course/main.js
@@ -9,9 +9,23 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+function classTech(event) {
+  const selectElement = document.querySelector('.tech')
+  selectElement.classList.remove('tech')
+  event.target.classList.add('tech')
+  
+}
+
+firstLi.addEventListener('click', classTech)
+secondLi.addEventListener('click', classTech)
+thirdLi.addEventListener('click', classTech)
+
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+// function changeElemnt(){
+
+// }
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
