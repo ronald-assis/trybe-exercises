36b828b86d453ce78931260c771a643da11e6a5f
Ronald Assis <ronaldassis71@gmail.com>
2021-09-17 12:02:37 -0300

    ponto

--- fundamentals/block-06_HTML-CSS/day-01/course/script.js
+++ fundamentals/block-06_HTML-CSS/day-01/course/script.js
@@ -110,3 +110,196 @@ function dateValidation(input, name) {
 
   return true
 }
+
+function getSelectedOption(select){
+  return select.options[select.selectedIndex];
+}
+
+function selectValidation(select, name){
+  let option = getSelectedOption(select);
+  let validation = inputs[name];
+
+  if(validation.required && (!option || option.disabled)){
+    return false;
+  }
+
+  return true;
+}
+
+function radioValidation(radio, name){
+  let checked = document.querySelector(`[name=${name}]:checked`)
+  
+  if(checked === null){
+    return false;
+  }
+
+  return true;
+}
+
+let validationStrategies = {
+  default: defaultValidation,
+  date: dateValidation,
+  select: selectValidation,
+  radio: radioValidation,
+}
+
+function validateInput(inputName){
+  let inputType = inputs[inputName].type;
+  let input = document.querySelector(`[name=${inputName}]`)
+
+  if(inputType){
+    let validationFunction = validationStrategies[inputType];
+    return validationFunction(input, inputName);
+  }
+
+  return validationStrategies.default(input, inputName);
+}
+  
+function renderErrorMessages(messages){
+  let form = document.querySelector('#cv-form');
+  let messageDiv = document.createElement('div');
+  messageDiv.className = 'error';
+  form.prepend(messageDiv);
+
+  for(let message of messages){
+    let p = document.createElement('p');
+    p.innerText = message;
+
+    messageDiv.appendChild(p);
+  }
+}
+
+function defaultRendering(input){
+  let p = document.createElement('p');
+  p.innerText = input.value;
+
+  return p;
+}
+
+function radioRendering(input){
+  let p = document.createElement('p');
+  let name = input.getAttribute('name');
+  let checked = document.querySelector(`[name=${name}]:checked`);
+
+  if(checked){
+    p.innerText = checked.value;
+  }
+
+  return p;
+}
+
+function selectRendering(input){
+  let p = document.createElement('p');
+  let option = getSelectedOption(input)
+  p.innerText = option.value;
+  
+  return p;
+}
+
+let renderStrategies = {
+  default: defaultRendering,
+  radio: radioRendering,
+  select: selectRendering,
+}
+
+function renderData(){
+  let dataDiv = document.createElement('div');
+  dataDiv.className = 'data';
+
+  let form = document.querySelector('#cv-form');
+  form.prepend(dataDiv);
+
+  for(let name in inputs){
+    let inputType = inputs[name].type;
+    let input = document.querySelector(`[name=${name}]`);
+
+    let element;
+
+    if(renderStrategies[inputType]){
+      element = renderStrategies[inputType](input, dataDiv)
+    } else {
+      element = renderStrategies.default(input, dataDiv)
+    }
+
+    dataDiv.appendChild(element);
+  }
+}
+
+function validateData(){
+  let validationsList = {};
+
+  for(let inputName in inputs){
+    let isValid = validateInput(inputName);
+    validationsList[inputName] = isValid;
+  }
+  
+  let counter = 0;
+  let messages = [];
+
+  for(let index in validationsList){
+    if(validationsList[index] === false){
+      counter += 1;
+    }
+
+    if(validationsList[index].message){
+      counter += 1;
+      messages.push(validationsList[index].message);
+    }
+  }
+
+  return {
+    errorQtd: counter,
+    messages,
+  }
+}
+
+function clearDivs(){
+  let errorDivs = document.querySelectorAll('.error');
+
+  for(div of errorDivs){
+    div.remove();
+  }
+
+  let dataDiv = document.querySelector('.data');
+
+  if(dataDiv){
+    dataDiv.remove();
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let validation = validateData();
+
+  clearDivs();
+  
+  if(validation.errorQtd === 0){
+    renderData();
+  } else {
+    validation.messages.unshift('Dados Inválidos')
+
+    renderErrorMessages(validation.messages)
+  }
+}
+
+function clearFields() {
+  let formElements = document.querySelectorAll('input');
+  let textArea = document.querySelector('textarea')
+  let div = document.querySelectorAll('.div-curriculum');
+  for (let index = 0; index < formElements.length && index < div.length; index += 1) {
+    let userInput = formElements[index];
+    userInput.value = '';
+    textArea.value = '';
+    div[index].innerText = '';
+  }
+}
+
+window.onload = function () {
+  createStateOptions();
+  let submitButton = document.querySelector('.submit-button');
+  submitButton.addEventListener('click', handleSubmit);
+
+  let clearButton = document.querySelector('.clear-button');
+  clearButton.addEventListener('click', clearFields)
+}
