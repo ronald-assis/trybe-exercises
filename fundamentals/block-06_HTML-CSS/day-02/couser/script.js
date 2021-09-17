d6e245d978e5c981a13d620c41f9476c06f76581
Ronald Assis <ronaldassis71@gmail.com>
2021-09-17 14:28:00 -0300

    course

---
+++ fundamentals/block-06_HTML-CSS/day-02/couser/script.js
@@ -0,0 +1,15 @@
+function createOptionStates() {
+  const select = document.querySelector('#state')
+  const states = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO']
+  
+  for (let i = 0; i < states.length; i += 1) {
+    let option = document.createElement('option')
+    
+    select.appendChild(option).innerHTML = states[i]
+    select.appendChild(option).value = states[i]
+  }
+}
+
+window.onload = () => {
+  createOptionStates()
+} 
