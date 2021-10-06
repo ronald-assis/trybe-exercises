b6bbf00a7f8ea656d5335f86b8fb921d31ff7f9b
Ronald Assis <ronaldassis71@gmail.com>
2021-10-06 09:54:46 -0300

    test

---
+++ fundamentals/block-08_Higher-Order-function/day-02/script.js
@@ -0,0 +1,9 @@
+
+
+
+const color = (color) => {
+  const COLORS = ["black","brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+  return COLORS.indexOf(color);
+}
+
+console.log(color('red'))
