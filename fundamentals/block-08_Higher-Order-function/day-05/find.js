4a32c5423515d90e2128614dd03508c31b7c4b5d
Ronald Assis <ronaldassis71@gmail.com>
2021-10-19 21:41:12 -0300

    add class files

--- fundamentals/block-08_Higher-Order-function/day-05/find.js
+++ fundamentals/block-08_Higher-Order-function/day-05/find.js
@@ -1,4 +1,5 @@
-const books = [
+const books = {
+  screen:[
   {
     id: 1,
     name: 'As Crônicas de Gelo e Fogo',
@@ -59,7 +60,7 @@ const books = [
     },
     releaseYear: 1928,
   },
-];
+]};
 
 // Adicione o código do exercício aqui:
 // 1- Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
---
+++ fundamentals/block-09_Asynchronous-test/day-03/live class/sd-016-a-live-lectures/github.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+
+const getUserReposUrl = (username) => `https://api.github.com/users/${username}`;
+
+const getGitHubUserInfos = async (username) => {
+  const url = getUserReposUrl(username);
+
+  const result = await fetch(url);
+  const data = await result.json();
+
+  const { name, company, twitter_user: twitter, bio, location } = data;
+  return {
+    name,
+    company,
+    twitter,
+    bio,
+    location,
+  };
+
+};
+
+getGitHubUserInfos('alexandremhm')
+  .then((data) => console.log(data));
+
+module.exports = {
+  getGitHubUserInfos,
+  getUserReposUrl,
+};
